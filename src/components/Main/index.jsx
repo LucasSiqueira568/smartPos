24357424c9d308f269c3146fc94beac1661ddde3
LucasSiqueira568 <siqueiralucas-pro@outlook.com>
2021-06-23 21:08:00 -0300

    Creation of the first project components

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../Main/app.module.scss'
+import MenuBar from '../Menu-Bar'
+
+export default function Main() {
+    return(
+        <div>
+            <MenuBar />
+        </div>
+    )
+}
---
+++ src/components/Menu-Bar/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './App.module.scss'
+
+
+export default function MenuBar() {
+    return(
+        <div className={styles.menuBar}>
+            <header className={styles.menuHeader}>
+                <ul>
+                    <li><a href="/">Dashboard</a></li>
+                    <li><a href="/">Comidas e Drinks</a></li>
+                    <li><a href="/">Mensagens</a></li>
+                    <li><a href="/">Cardápio</a></li>
+                    <li><a href="/">Configurações</a></li>
+                </ul>
+            </header>
+        </div>
+    )
+}
